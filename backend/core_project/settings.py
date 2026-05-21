0b582ba357dc51b07550e83137a5df12440d7996
Caio Vilas Boas Miranda <caiovbm512@gmail.com>
2026-05-21 18:59:35 -0300

    chore: configurando django para producao no render

--- backend/core_project/settings.py
+++ backend/core_project/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 # Constroem os caminhos dentro do projeto como: BASE_DIR / 'subdir'.
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
 # Middlewares (Eles resolvem os erros E408, E409 e E410 do seu print)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,14 +61,11 @@ WSGI_APPLICATION = 'core_project.wsgi.application'
 
 # Banco de Dados 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'worldcup2026',
-        'USER': 'admin',
-        'PASSWORD': 'adminpassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Avisa o Django para usar o seu modelo de Usuário customizado!
@@ -88,11 +87,10 @@ USE_TZ = True
 
 # Arquivos estáticos
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Tipo do campo de ID padrão
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Libera o Next.js para consumir a API
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
